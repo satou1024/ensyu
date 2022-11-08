3bc296b1e904b79be9741bddbe488920a51e70ef
satou1024 <taku.satou.sys22@morijyobi.ac.jp>
2022-11-08 10:35:43 +0900

    2人分の計算を可能にしました

--- Main.java
+++ Main.java
@@ -2,13 +2,16 @@ import java.util.Scanner;
 
 public class Main{
 	public static void main(String[] args) {
-        System.out.println("Weight:");
-        double Weight = new Scanner(System.in).nextDouble();
-        System.out.println("Height:");
-        double Height = new Scanner(System.in).nextDouble();
-        double BMI = Weight / (Height * Height);
-        String sv = String.format("%.2f", BMI);
-        double v2 = Double.parseDouble(sv);
-        System.out.println(v2);
+        for (int i = 0; i < 2; i++) {
+            System.out.println(i + 1);   
+            System.out.println("Weight:");
+            double Weight = new Scanner(System.in).nextDouble();
+            System.out.println("Height:");
+            double Height = new Scanner(System.in).nextDouble();
+            double BMI = Weight / (Height * Height);
+            String sv = String.format("%.2f", BMI);
+            double v2 = Double.parseDouble(sv);
+            System.out.println(v2);
+        }
     }
 }
